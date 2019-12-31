2c54aebe4157bcc77ba90e0766a8c13a20a73ee0
Bin-Cheng-THU <34901368+Bin-Cheng-THU@users.noreply.github.com>
2019-12-31 16:45:33 -0700

    DEMBody 6.3
    Fix some bugs.

--- src/global.F90
+++ src/global.F90
@@ -31,7 +31,7 @@
     !  Parameters
     real(8),parameter :: GravConst = 6.674184D-11
     real(8),parameter :: PI = 3.141592653589793D0
-    character(10),parameter :: VERSION = '6.2'
+    character(10),parameter :: VERSION = '6.3'
 
     !  Define control parameters of Program
     character(10) :: vsDEMBody
